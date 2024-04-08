506869bc2d7627b10d0c4918ee6d8f3f7df8f034
Kumar9525 <92426822+Kumar9525@users.noreply.github.com>
2024-04-08 14:23:01 +0530

    created a submission

--- script.js
+++
@@ -1,9 +0,0 @@
-describe("Increment Counter", () => {
-  const baseUrl = "http://localhost:3000"; // assuming the app is running locally on port 3000
-
-  it("increments the counter when the button is clicked", () => {
-    cy.visit(baseUrl);
-    cy.get('#incrementBtn').click();
-    cy.get('#counter').should('have.text', '1');
-  });
-});
